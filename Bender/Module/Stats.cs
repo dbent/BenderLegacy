1289bfd5724892560166e40ffd589c275b6af79c
Dwayne Bent <dbb@dbb.io>
2012-12-09 11:46:47 -0500

    Add new Stats module
    Supports uptime command.

---
+++ Bender/Module/Stats.cs
@@ -0,0 +1,42 @@
+﻿using Bender.Configuration;
+using Bent.Common.Extensions;
+using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Bender.Module
+{
+    [Export(typeof(IModule))]
+    public class Stats : IModule
+    {
+        private static Regex regexUptime = new Regex(@"^\s*uptime\s*$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        private IConfiguration config;
+        private IBackend backend;
+
+        public void OnStart(IConfiguration config, IBackend backend)
+        {
+            this.config = config;
+            this.backend = backend;
+        }
+
+        public void OnMessage(IMessage message)
+        {
+            if (message.IsRelevant)
+            {
+                TryUptime(message);
+            }
+        }
+
+        private void TryUptime(IMessage message)
+        {
+            var match = regexUptime.Match(message.Body);
+            if (match.Success)
+            {
+                var startTime = Process.GetCurrentProcess().StartTime;
+                this.backend.SendMessageAsync(message.ReplyTo, @"I have been running since {0} ({1}).".FormatWith(startTime, DateTime.Now - startTime));
+            }
+        }
+    }
+}
