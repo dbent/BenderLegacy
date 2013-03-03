636998625a740147c5d9d7cd19f1f8f27bf34344
Dwayne Bent <dbb@dbb.io>
2013-03-03 13:45:05 -0500

    Add IKeyValuePersistence interface
    --HG--
    extra : rebase_source : fff419c422b5841712c317cb4a13874406985ee3

---
+++ Bender/Persistence/IKeyValuePersistence.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bender.Persistence
+{
+    public interface IKeyValuePersistence
+    {
+        string Get(string key);
+        void Set(string key, string value);
+    }
+}
